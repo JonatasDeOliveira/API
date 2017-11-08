e003c7aca2fb58e4d4c9f8b9d0e5dc7a1d2bb637
Jonatas Oliveira <joc@cin.ufpe.br>
2017-11-08 21:57:18 +0000

    corrigindo módulo thesaurus

--- lexer/thesaurus.py
+++ lexer/thesaurus.py
@@ -1,11 +1,14 @@
 # encoding: utf-8
 import json
 
+def _removeDuplicates(query):
+    result = []
+    for q in query:
+       if q not in result:
+           result.append(q)
+    return result
 
-_ls = []
-_la = []
-
-def load(word):
+def _load(word):
     c = word[0]
     path = "lexer/thesaurus/thesaurus_update/" + c + ".json"
     with open(path, 'r') as fileIn:
@@ -14,10 +17,9 @@ def load(word):
     return syn
 
 def synonyms(word):
-    global _ls
-    _ls = []
+    ls = []
     
-    syn = load(word)
+    syn = _load(word)
     dWord = syn.get(word)
     if dWord is not None:
         ctxKeys = dWord.keys()
@@ -28,20 +30,19 @@ def synonyms(word):
                 if isinstance(dCtx,dict):
                     dSyn = dCtx.get("synonyms")
                     if dSyn.has_key(str(relev)):
-                        _ls.extend(dSyn.get(str(relev)))
+                        ls.extend(dSyn.get(str(relev)))
                 else:
-                    _ls.extend(synonyms(dCtx.encode("utf-8")))
+                    ls.extend(synonyms(dCtx.encode("utf-8")))
                     relev = 0
             relev -= 1
     
-    return _ls
+    return _removeDuplicates(ls)
 
 
 def antonyms(word):
-    global _la
-    _la = []
+    la = []
     
-    syn = load(word)
+    syn = _load(word)
     dWord = syn.get(word)
     if dWord is not None:
         ctxKeys = dWord.keys()
@@ -52,10 +53,11 @@ def antonyms(word):
                 if isinstance(dCtx,dict):
                     dSyn = dCtx.get("antonyms")
                     if dSyn.has_key(str(relev)):
-                        _la.extend(dSyn.get(str(relev)))
+                        la.extend(dSyn.get(str(relev)))
                 else:
-                    _la.extend(antonyms(dCtx.encode("utf-8")))
+                    la.extend(antonyms(dCtx.encode("utf-8")))
                     relev = 0
             relev -= 1
     
-    return _la
+    return _removeDuplicates(la)
+    
